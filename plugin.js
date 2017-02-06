97a9b2d455a0d1b98e3a7af3f14aeba0f6d6edec
ruslana-shev <ruslana.shevandina@gmail.com>
2017-02-06 04:15:55 +0200

    исправлены замечания, добавлена форма для поиска по сайту

--- plugin.js
+++ plugin.js
@@ -8,13 +8,16 @@
 		moreOptionBtn.toggleClass('open');
 	});
 }) (jQuery);
-/*;(function ($){
-	var serchIcBtn = $('.serch-ic-btn');
-	var serchIcForm = $('.serch-ic-form');
 
-	moreOptionBtn.on('click', function () {
-		moreOptionForm.slideToggle();
-		moreOptionForm.toggleClass('open');
-		moreOptionBtn.toggleClass('open');
+;(function ($){
+	var searchIcBtn = $('.search-ic-btn');
+	var searchIcForm = $('.search-ic-form');
+	var searchOnSite = $('serch-on-site')
+
+	searchIcBtn.on('click', function () {
+		searchIcForm.slideToggle();
+		searchIcForm.toggleClass('open');
+		searchIcBtn.toggleClass('open');
 	});
-}) (jQuery);*/
+	
+}) (jQuery);
